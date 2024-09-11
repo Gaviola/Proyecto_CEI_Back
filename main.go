c98b462a0c83c79622ef3a7357ff587612ceae93
ElPelado619 <agustinyornet@gmail.com>
2024-09-11 19:51:17 -0300

    Added db queries to retrieve item and item types

--- main.go
+++ main.go
@@ -1,23 +1,26 @@
 package main
 
 import (
+	"strings"
+
 	"github.com/Gaviola/Proyecto_CEI_Back.git/internal/configs"
 	"github.com/Gaviola/Proyecto_CEI_Back.git/internal/logger"
 	"github.com/Gaviola/Proyecto_CEI_Back.git/internal/services"
 	"github.com/Gaviola/Proyecto_CEI_Back.git/routes"
+	"github.com/Gaviola/Proyecto_CEI_Back.git/utils"
 	"github.com/dgrijalva/jwt-go"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
-	"strings"
 
 	"crypto/rand"
 	"encoding/base64"
 	"fmt"
-	"github.com/Gaviola/Proyecto_CEI_Back.git/data"
 	"log"
 	"net/http"
 	"os"
 	"time"
+
+	"github.com/Gaviola/Proyecto_CEI_Back.git/data"
 
 	_ "github.com/lib/pq"
 
@@ -47,22 +50,64 @@ func main() {
 	services.InitializeOAuthGoogle()
 	fmt.Println("OAuth2 Services initialized...")
 
-	//Crear llave secreta
-	key := make([]byte, 64)
-	_, err := rand.Read(key)
-	if err != nil {
-		log.Fatal(err)
-	}
-	secret := base64.StdEncoding.EncodeToString(key)
-	err = os.Setenv("JWT_SECRET", secret)
-	if err != nil {
-		http.Error(nil, "Error al setear la variables de entorno", http.StatusInternalServerError)
-		return
-	}
+	var opcion int
+	fmt.Println("DEBUG MENU")
+	fmt.Println("[1] \tSetear variables de entorno")
+	fmt.Println("[2] \tGoogle Login")
+	fmt.Println("[3] \tPrueba de ItemTypes")
+	fmt.Println("[4] \tPrueba de Items")
+	fmt.Println("[OTRO] \tSalir")
 
-	fmt.Println("Servidor escuchando en http://localhost:8080")
-	logger.Log.Info("Started running on http://localhost:" + viper.GetString("port"))
-	log.Fatal(http.ListenAndServe(":"+viper.GetString("port"), nil))
+	fmt.Print("> Ingrese una opción: ")
+	fmt.Scan(&opcion)
+	fmt.Println()
+
+	switch opcion {
+	case 1:
+		fmt.Println("Has seleccionado la opción 1")
+		//Crear llave secreta
+		key := make([]byte, 64)
+		_, err := rand.Read(key)
+		if err != nil {
+			log.Fatal(err)
+		}
+		secret := base64.StdEncoding.EncodeToString(key)
+		err = os.Setenv("JWT_SECRET", secret)
+		if err != nil {
+			http.Error(nil, "Error al setear la variables de entorno", http.StatusInternalServerError)
+			return
+		}
+
+		fmt.Println("Servidor escuchando en http://localhost:8080")
+		logger.Log.Info("Started running on http://localhost:" + viper.GetString("port"))
+		log.Fatal(http.ListenAndServe(":"+viper.GetString("port"), nil))
+		
+	case 2:
+		// Routes for the application
+		http.HandleFunc("/", services.HandleMain)
+		http.HandleFunc("/login-gl", services.HandleGoogleLogin)
+		http.HandleFunc("/callback-gl", services.CallBackFromGoogle)
+
+		log.Fatal(http.ListenAndServe(":8080", nil))
+
+		fmt.Println("Servidor escuchando en http://localhost:8080")
+		logger.Log.Info("Started running on http://localhost:" + viper.GetString("port"))
+		log.Fatal(http.ListenAndServe(":"+viper.GetString("port"), nil))
+
+	case 3:
+		itemTypes := utils.DBShowItemTypes()
+		for _, itemType := range itemTypes {
+			fmt.Println(itemType)
+		}
+
+	case 4:
+		items := utils.DBShowItems()
+		for _, item := range items {
+			fmt.Println(item)
+		}
+	default:
+		fmt.Println("Saliendo...")
+	}
 
 }
 
--- utils/db.go
+++ utils/db.go
@@ -14,8 +14,14 @@ import (
 Conecta a la base de datos y devuelve un puntero a la conexion.
 Devuelve nil si hay un error en la conexion.
 */
-func connect() *sql.DB {
-	connStr := "host=localhost dbname=CEI user=fgaviola password=facu1234 sslmode=disable"
+func connect(isFacu bool) *sql.DB {
+	var connStr string
+
+	if isFacu {
+		connStr = "host=localhost dbname=CEI user=fgaviola password=facu1234 sslmode=disable"
+	} else {
+		connStr = "host=localhost dbname=cei user=agus password=0811 sslmode=disable"
+	}
 
 	db, err := sql.Open("postgres", connStr)
 	if err != nil {
@@ -40,7 +46,7 @@ Devuelve un usuario vacio si el usuario no existe o si hay un error en la base d
 */
 func DBExistUser(PassHash []byte, user string) (data.User, error) {
 	findUser := data.User{}
-	db := connect()
+	db := connect(false)
 	// TODO modificar la query para que busque por el hash y el username
 	//query := "SELECT * FROM users WHERE name = 'facu'"
 	query := "SELECT * FROM users WHERE name = '" + user + "' AND hash = '" + string(PassHash) + "'"
@@ -50,4 +56,49 @@ func DBExistUser(PassHash []byte, user string) (data.User, error) {
 		return findUser, result
 	}
 	return findUser, nil
+}
+
+func DBShowItemTypes() []data.ItemType {
+	var itemTypes []data.ItemType
+
+	db := connect(false)
+	query := "SELECT * FROM item_type"
+	rows, err := db.Query(query)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close()
+	for rows.Next() {
+		var itemType data.ItemType
+		err := rows.Scan(&itemType.ID, &itemType.Name, &itemType.IsGeneric)
+		if err != nil {
+			log.Fatal(err)
+		}
+		itemTypes = append(itemTypes, itemType)
+	}
+	return itemTypes
+}
+
+func DBShowItems() []data.Item {
+	var items []data.Item
+
+	db := connect(false)
+	query := "select it.id, it.name, e.code, e.price from element e join item_type it on e.type_id = it.id;"
+	
+	rows, err := db.Query(query)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close()
+	for rows.Next() {
+		var item data.Item
+		err := rows.Scan(&item.ID, &item.ItemType, &item.Code, &item.Price)
+		if err != nil {
+			log.Fatal(err)
+		}
+		items = append(items, item)
+	}
+
+	return items
 }
