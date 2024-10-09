c673b7b595ab77e262afe582f2922554a2074377
ElPelado619 <agustinyornet@gmail.com>
2024-10-09 17:54:55 -0300

    Corregido un error en la struct Item

--- models/item.go
+++ models/item.go
@@ -5,9 +5,9 @@ package models
 Estructura de datos de un ítem.
 */
 type Item struct {
-	ID       int    `json:"id"`
-	ItemType string `json:"item_type"`
-	ItemTypeID int 
-	Code     string 	`json:"code"`
-	Price    float64 `json:"price"`
+	ID         int 		`json:"id"`
+	ItemType   string	`json:"item_type"`
+	ItemTypeID int     `json:"item_type_id"`
+	Code       string  `json:"code"`
+	Price      float64 `json:"price"`
 }
